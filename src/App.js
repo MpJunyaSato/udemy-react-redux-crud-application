17ffc8417e5c24d88a16923480ba94fdbadcc3a9
Junya Sato <sato@metaps-payment.com>
2020-04-26 14:05:27 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
 
+// class App extends Component {
+//   render() {
+//     const greeting = 'Hi, Tom!'
+//     const dom = <h1 className="foo">{greeting}</h1>
+//     return dom;
+//   }
+// }
+// class App extends Component {
+//   render() {
+//     return <input type="text" onChange={() => {console.log("I am clicked")}} />;
+//   }
+// }
 class App extends Component {
   render() {
-    return <h1>Hello, World!</h1>;
+    return (
+        <React.Fragment>
+          <label htmlFor="bar">bar</label>
+          <input type="text" onChange={() => {console.log("I am clicked")}} />
+        </React.Fragment>
+    )
   }
 }
+// class App extends Component {
+//   render() {
+//     return <div>Hello, World!</div>;
+//   }
+// }
 // class App extends Component {
 //   render() {
 //     return React.createElement(
