6e2f23c3cccb2d7ed7ca2512bf386984c94e1284
Junya Sato <sato@metaps-payment.com>
2020-04-29 14:46:55 +0900

    implement events index

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./components/App";
+import EventsIndex from "./components/events_index";
 import * as serviceWorker from './serviceWorker';
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from 'react-redux'
 import reducer from './reducers'
+import thunk from 'redux-thunk'
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventsIndex />
   </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
+// If you want your EventsIndex to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
